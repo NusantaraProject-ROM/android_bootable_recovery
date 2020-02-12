68b5bddcc1de98897811e98ec9037a6b239de5d3
Nolen Johnson <johnsonnolen@gmail.com>
2020-02-12 00:03:01 +0000

    recovery: Mark as Lineage Recovery
    * So many users think they accidetally restored the stock
      Recovery right now, as we haven't brough our custom UI
      up yet. Make it easier to discern.
    
    Change-Id: Ic803be129e30810c9832a31d0067d981cd9795b3

--- recovery_ui/include/recovery_ui/screen_ui.h
+++ recovery_ui/include/recovery_ui/screen_ui.h
@@ -148,7 +148,7 @@ class TextMenu : public Menu {
   size_t MenuEnd() const;
 
   // Menu example:
-  // info:                           Android Recovery
+  // info:                           Lineage Recovery
   //                                 ....
   // help messages:                  Swipe up/down to move
   //                                 Swipe left/right to select
